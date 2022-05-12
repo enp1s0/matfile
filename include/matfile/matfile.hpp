8781f47023f4fe13884490b9335ac2da146e50e7
mutsuki <mutsuki@momo86.net>
2022-05-12 23:18:58 +0900

    Add matrix market file loader

--- include/matfile/matfile.hpp
+++ include/matfile/matfile.hpp
@@ -1,6 +1,7 @@
 #ifndef __MATFILE_HPP__
 #define __MATFILE_HPP__
 #include <fstream>
+#include <sstream>
 #include <stdexcept>
 
 namespace mtk {
@@ -130,6 +131,148 @@ void load_dense(
 	}
 	ifs.close();
 }
+
+namespace matrix_market {
+namespace detail {
+using matrix_type_t = unsigned;
+constexpr matrix_type_t unsupported_matrix = 0;
+constexpr matrix_type_t general_matrix   = 1;
+constexpr matrix_type_t symmetric_matrix = 2;
+
+using element_type_t = unsigned;
+constexpr element_type_t unsupported_value = 0;
+constexpr element_type_t real_value = 1;
+constexpr element_type_t pattern_value = 2;
+
+inline matrix_type_t get_matrix_type(
+		const std::string banner
+		) {
+	if (banner.find("general") != std::string::npos) {
+		return general_matrix;
+	} else if (banner.find("symmetric") != std::string::npos) {
+		return symmetric_matrix;
+	}
+	return unsupported_matrix;
+}
+
+inline element_type_t get_element_type(
+		const std::string banner
+		) {
+	if (banner.find("real") != std::string::npos) {
+		return real_value;
+	} else if (banner.find("pattern") != std::string::npos) {
+		return pattern_value;
+	}
+	return unsupported_value;
+}
+} // unnamed namespace
+
+template <class INT_T>
+void load_matrix_size(
+		INT_T& m,
+		INT_T& n,
+		const std::string filepath
+		) {
+	std::ifstream ifs(filepath);
+
+	if (!ifs) {
+		throw std::runtime_error("[matfile error] No such file : " + filepath);
+	}
+
+	std::string line;
+	while (std::getline(ifs, line)) {
+		if (line[0] != '%') {
+			break;
+		}
+	}
+	std::size_t num_elements;
+
+	std::stringstream ss(line);
+	ss >> m >> n >> num_elements;
+
+	ifs.close();
+}
+
+template <class INT_T = std::size_t>
+std::pair<INT_T, INT_T> load_matrix_size(
+		const std::string filepath
+		) {
+	std::size_t m, n;
+	load_matrix_size(m, n, filepath);
+
+	return std::pair<INT_T, INT_T>{m, n};
+}
+
+template <class T>
+void load_matrix(
+		T* const ptr,
+		const std::size_t ld,
+		const std::string filepath,
+		const bool fill_zero = true
+		) {
+	std::ifstream ifs(filepath);
+
+	if (!ifs) {
+		throw std::runtime_error("[matfile error] No such file : " + filepath);
+	}
+
+	std::string line;
+	std::getline(ifs, line);
+
+	const auto matrix_type = detail::get_matrix_type(line);
+	if (matrix_type == detail::unsupported_matrix) {
+		throw std::runtime_error("Unsupported matrix type : banner = " + line);
+	}
+	const auto element_type = detail::get_element_type(line);
+	if (matrix_type == detail::unsupported_matrix) {
+		throw std::runtime_error("Unsupported element type : banner = " + line);
+	}
+
+	while (std::getline(ifs, line)) {
+		if (line[0] != '%') {
+			break;
+		}
+	}
+	std::size_t m, n, num_elements;
+
+	std::stringstream ss(line);
+	ss >> m >> n >> num_elements;
+
+	if (fill_zero) {
+		for (std::size_t i = 0; i < m; i++) {
+			for (std::size_t j = 0; j < n; j++) {
+				ptr[i + j * ld] = 0;
+			}
+		}
+	}
+
+	for (std::size_t l = 0; l < num_elements; l++) {
+		if (element_type == detail::real_value) {
+			std::size_t i, j;
+			double v;
+			ifs >> i >> j >> v;
+			i--;j--;
+
+			ptr[i + j * ld] = v;
+
+			if (matrix_type == detail::symmetric_matrix) {
+				ptr[j + i * ld] = v;
+			}
+		} else if (element_type == detail::pattern_value) {
+			std::size_t i, j;
+			ifs >> i >> j;
+			i--;j--;
+
+			ptr[i + j * ld] = 1;
+
+			if (matrix_type == detail::symmetric_matrix) {
+				ptr[j + i * ld] = 1;
+			}
+		}
+	}
+	ifs.close();
+}
+} // namespace matrix_market
 } // namespace matfile
 } // namespace mtk
 #endif
